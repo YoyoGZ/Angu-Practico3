eb84e60f371328e2168c9dd6674363324257e8a9
YoyoGZ <117695561+YoyoGZ@users.noreply.github.com>
2023-10-09 19:12:17 -0300

    Forms - 2do commit

--- src/app/form-rf/form-rf.component.ts
+++ src/app/form-rf/form-rf.component.ts
@@ -19,6 +19,19 @@ export class FormRFComponent {
       city: [''],
     });
 }
+get emailControl() {
+  return (this.userForm.controls['email'])
+}
+get emailControlIsInvalid () {
+  return (this.emailControl.invalid && this.emailControl.touched)
+}
+
+get passwordControl() {
+  return (this.userForm.controls['password'])
+}
+get passwordControlIsInvalid () {
+  return (this.passwordControl.touched)
+}
 
 onSubmit(): void {
       console.log(this.userForm.value)
